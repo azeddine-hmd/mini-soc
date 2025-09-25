1c3f837d652220ac44b5b3ac0ee894480a3a1418
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2025-09-25 23:12:08 +0100

    generating ssh keys before creating vagrant vms

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-vagrant up
+ansible-playbook ansible/deploy-nodes-ssh.yml
+cd vagrant; vagrant up
 ansible-galaxy collection install community.docker
 ansible-playbook -i ansible/inventory.ini ansible/deploy-wazuh.yml
