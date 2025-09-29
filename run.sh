860a777b52de074a8535afcdd472ac19d2a9f9d7
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2025-09-29 06:09:29 +0100

    removing uncessary exports since we are using ansible facts

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-export MY_UID="$(id -u)"
-export MY_GID="$(id -g)"
 ansible-galaxy collection install community.docker || exit 1
 ansible-playbook -i ansible/inventory.ini ansible/deploy-wazuh.yml || exit 1
