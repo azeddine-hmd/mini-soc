fc75a845d4b8aa5c887e0359b691dfceaefb16fa
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2025-09-29 06:06:39 +0100

    fix permission for ssl genreated keys as well as removing automatic restart for services

---
+++ stack/my-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Execute native entrypoint..."
+chmod +x /entrypoint.sh
+/entrypoint.sh
+
+echo "Setting proper permissions on certificate directory..."
+echo "UID: ${MY_UID} and GID ${MY_GID}"
+chmod -R 755 /certificates/
+chown -R $MY_UID:$MY_GID /certificates/
